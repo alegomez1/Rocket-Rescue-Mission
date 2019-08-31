fc939bda8f24b75cd792e3a3efced3718d8b60e3
alejandrogomez <alegomez1@me.com>
2019-08-31 14:26:14 -0400

    Implemented Phaser.js and began game work

---
+++ Javascript/script.js
@@ -0,0 +1,15 @@
+console.log("Script is connected")
+
+const game = new Phaser.Game(800, 600, Phaser.AUTO, '',{
+    preload: preload,
+    create: create,
+    update, update
+})
+
+function preload() {}
+
+function create() {}
+
+function update() {}
+    
+    
