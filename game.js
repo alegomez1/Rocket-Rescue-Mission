acf21fb8d0b70813aaed5f94c8c1a230e93a80a9
alejandrogomez <alegomez1@me.com>
2019-09-04 12:39:35 -0400

    Added movement to the rocket and gravity

--- game.js
+++ game.js
@@ -6,12 +6,12 @@ const game = new Phaser.Game(1300, 500, Phaser.AUTO, '', {
 
 let player
 
-function preload(){
+function preload() {
   game.load.image('rocket', "images/RocketSprite.png")
 
 }
 
-function create(){
+function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
   player = game.add.sprite(0, 0, 'rocket')
 
@@ -22,8 +22,28 @@ function create(){
   player.body.gravity.y = 800
   //Stops player from falling||Allows it to stay within world bounds
   player.body.collideWorldBounds = true
+
+  cursors = game.input.keyboard.createCursorKeys()
 }
 
-function update(){
-  
+function update() {
+
+  //Player's movement along x-axis||Can be altered to simulate wind blowing conditions
+  player.body.velocity.x = 0
+
+
+  //Movement
+  if (cursors.left.isDown) {
+    player.body.velocity.x = -150
+    player.animations.play('left')
+  } else if (cursors.right.isDown) {
+    player.body.velocity.x = 150
+    player.animations.play('right')
+  } if (cursors.up.isDown) {
+    player.body.velocity.y = -450
+  } else {
+    player.animations.stop()
+  }
+
+
 }
