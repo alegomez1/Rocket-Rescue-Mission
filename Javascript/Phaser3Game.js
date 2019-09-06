d06dbf2b3d72c35ef85d04b2b1aae1874cfcb4ad
alejandrogomez <alegomez1@me.com>
2019-09-06 09:40:35 -0400

    Created Phaser 3 scncene and config

--- Javascript/Phaser3Game.js
+++ Javascript/Phaser3Game.js
@@ -1,2 +1,26 @@
 /** @type {import("../Typings/phaser*/
 
+// import {LoadScene} from "./scenes/LoadScene";
+// import {MenuScene} from "./scenes/MenuScene";
+
+console.log("Working")
+
+var config = {
+    type: Phaser.AUTO,
+    width: 1300,
+    height: 500,
+    scene: {
+        preload: preload,
+        create: create
+    }
+}
+
+var game = new Phaser.Game(config);
+
+function preload(){
+
+}
+
+function create() {
+
+}
