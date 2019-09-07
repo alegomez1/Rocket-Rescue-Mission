d99e979444df1977eeb1c5e1b2ff65d949da4bc4
alejandrogomez <alegomez1@me.com>
2019-09-06 23:47:08 -0400

    Adding more scenes

--- public/Scenes/TitleScene.js
+++ public/Scenes/TitleScene.js
@@ -2,4 +2,13 @@ class TitleScene extends Phaser.Scene {
     constructor(){
         super({key: "TitleScene"})
     }
+    preload(){
+        this.load.image('sky', '../images/sky.png')
+    }
+    create(){
+        let bg = this.add.sprite(0, 0, 'bg')
+        bg.setOrigin(0,0)
+    }
 }
+
+export default TitleScene;
