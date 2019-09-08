7aef97b6f0e21afe3a8a34f81ba17e4cfe90a882
alejandrogomez <alegomez1@me.com>
2019-09-08 18:33:35 -0400

    Added how to play section

--- src/Scenes/menuScene.js
+++ src/Scenes/menuScene.js
@@ -10,11 +10,30 @@ export default class menuScene extends Phaser.Scene {
         
     }
     create(){
-        button = this.add.text(20,300, 'START GAME', {
+
+        //How to play section
+        this.add.text(55, 16, 'How To Play', {
+            fontSize: "50px",
+            fill: "#FFFFFF",
+        })
+        this.add.text(30, 100, "- Left/Right Arrows: Turn ship left/right", {
+            fontSize: "20px",
+            fill: "#FFFFFF",
+        })
+        this.add.text(30, 150, "- Up Arrow: Move ship in that direction", {
+            fontSize: "20px",
+            fill: "#FFFFFF",
+        })
+
+
+
+
+
+        //Start Game Button
+        button = this.add.text(1000,300, 'START GAME', {
             fontSize: '30px',
             fill: '#FFFFFF'
         })
-
         button.setInteractive().on("pointerover", () => {
             document.body.style.cursor = "pointer";
             button.alpha = 1;
